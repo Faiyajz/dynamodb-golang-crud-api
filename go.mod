e9b68b4f5d177474ee8056b551c26c19791336c5
faiyaj <faiyajz007@gmail.com>
2022-11-07 11:04:47 +0600

    routes outline created

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module dynamodb-golang-crud-bulletproof
 
 go 1.19
-
-require github.com/go-chi/cors v1.2.1 // indirect
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,7 +1,51 @@
 package routes
 
-import()
+import "github.com/go-chi/chi"
 
-func NewRouter(){
-	
+type Router struct {
+	config *Config
+	router *chi.Mux
+}
+
+func NewRouter() *Router {
+	return &Router{
+		config: NewConfig().SetTimeout(serviceConfig.GetConfig().Timeout),
+		router: chi.NewRouter,
+	}
+}
+
+func (r *Router) SetRouters() *chi.Mux {
+
+}
+
+func (r *Router) setConfigsRouters() {
+
+}
+
+func RouterHealth() {
+
+}
+
+func RouterProduct() {
+
+}
+
+func EnableTimeout() {
+
+}
+
+func EnableCORS() {
+
+}
+
+func EnableRecover() {
+
+}
+
+func EnableRequestID() {
+
+}
+
+func EnableRealIP() {
+
 }
